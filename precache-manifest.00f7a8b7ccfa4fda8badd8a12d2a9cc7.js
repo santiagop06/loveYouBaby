9a06b6082b7fa4f2418ae4dbdc8d7207223643ef
santiagop06 <santiagopuentes8@gmail.com>
2020-10-16 01:59:31 -0500

    Updates

---
+++ precache-manifest.00f7a8b7ccfa4fda8badd8a12d2a9cc7.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1995c59bda3b2c1c84af613d5d327d8a",
+    "url": "/loveYouBaby/index.html"
+  },
+  {
+    "revision": "055eadb32bbdc4d1fef0",
+    "url": "/loveYouBaby/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "a24f0c465eea8c152c78",
+    "url": "/loveYouBaby/static/css/main.325e5c5d.chunk.css"
+  },
+  {
+    "revision": "055eadb32bbdc4d1fef0",
+    "url": "/loveYouBaby/static/js/2.773f7153.chunk.js"
+  },
+  {
+    "revision": "70af1bffd51a7281193fa8f9b7319597",
+    "url": "/loveYouBaby/static/js/2.773f7153.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a24f0c465eea8c152c78",
+    "url": "/loveYouBaby/static/js/main.caeb234f.chunk.js"
+  },
+  {
+    "revision": "4323b42656aad8fe8720",
+    "url": "/loveYouBaby/static/js/runtime-main.506277c1.js"
+  },
+  {
+    "revision": "3f730fdefea548689738fdf7a27eb9f5",
+    "url": "/loveYouBaby/static/media/Dark-white-in-Basic4.3f730fde.png"
+  },
+  {
+    "revision": "a6abc16bb3b89de3067fdd4c14c1aa01",
+    "url": "/loveYouBaby/static/media/ToyFaces_Tansparent_BG_56.a6abc16b.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/loveYouBaby/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b41756a5606b4fd479dd63bd7539eaf7",
+    "url": "/loveYouBaby/static/media/question1.b41756a5.jpeg"
+  },
+  {
+    "revision": "ea2a0df5f4db6a0a09ff01c5c5651dd7",
+    "url": "/loveYouBaby/static/media/question3.ea2a0df5.jpeg"
+  }
+]);
